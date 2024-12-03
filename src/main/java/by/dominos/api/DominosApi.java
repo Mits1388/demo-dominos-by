fd57b2c992e918972f5b38315c938c94a8e736a4
Mits16 <mits1388@gmail.com>
2024-12-03 20:34:52 +0300

    add log4j2-test.xml

--- src/main/java/by/dominos/api/DominosApi.java
+++ src/main/java/by/dominos/api/DominosApi.java
@@ -2,12 +2,19 @@ package by.dominos.api;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import static io.restassured.RestAssured.given;
 
 public class DominosApi {
 
+    private static final Logger logger = LogManager.getLogger();
+
     public ValidatableResponse getResponsePhone(String phone) {
+
+        logger.info("DominosApi - getResponsePhone");
+
         ValidatableResponse response =
                 given()
                     .body(LoginRequest.getBody(phone))
--- src/test/java/by/dominos/api/LoginTest.java
+++ src/test/java/by/dominos/api/LoginTest.java
@@ -1,45 +1,52 @@
 package by.dominos.api;
 
 import io.restassured.response.ValidatableResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static org.hamcrest.Matchers.equalTo;
 
 public class LoginTest {
+    private static final Logger logger = LogManager.getLogger();
 
     @Test
     @DisplayName("тест: вход без ввода номера телефона")
     public void noPhone() {
-
+        logger.info("Начало теста: noPhone() - вход без ввода номера телефона");
         ValidatableResponse response = new DominosApi()
                 .getResponsePhone(String.valueOf(Phone.EMPTY_PHONE.getLabel()));
-
+        logger.info("LoginMessage.LOGIN_EMPTY_NUMBER_PHONE = " + LoginMessage.LOGIN_EMPTY_NUMBER_PHONE);
         response
                 .statusCode(400)
                 .body("phone[0]", equalTo(LoginMessage.LOGIN_EMPTY_NUMBER_PHONE));
+        logger.info("Конец теста: noPhone() - вход без ввода номера телефона");
     }
 
     @Test
     @DisplayName("тест: вход с некоректным номером телефона")
     public void incorrectPhone() {
-
+        logger.info("Начало теста: incorrectPhone() - вход с некоректным номером телефона");
         ValidatableResponse response = new DominosApi()
                 .getResponsePhone(String.valueOf(Phone.INCORRECT_PHONE.getLabel()));
-
+        logger.info("LoginMessage.LOGIN_INCORRECT_NUMBER_PHONE = " + LoginMessage.LOGIN_INCORRECT_NUMBER_PHONE);
         response
                 .statusCode(400)
                 .body("phone[0]", equalTo(LoginMessage.LOGIN_INCORRECT_NUMBER_PHONE));
+        logger.info("Конец теста: incorrectPhone() - вход с некоректным номером телефона");
     }
 
     @Test
     @DisplayName("тест: вход с коректным номером телефона")
     public void correctPhone() {
-
+        logger.info("Начало теста: correctPhone() - вход с коректным номером телефона");
         ValidatableResponse response = new DominosApi()
                 .getResponsePhone(String.valueOf(Phone.CORRECT_PHONE.getLabel()));
-
+        logger.info("LoginMessage.LOGIN_CORRECT_NUMBER_PHONE = " + LoginMessage.LOGIN_CORRECT_NUMBER_PHONE);
         response
                 .statusCode(200)
                 .body("text", equalTo(LoginMessage.LOGIN_CORRECT_NUMBER_PHONE));
+        logger.info("Конец теста: correctPhone() - вход с коректным номером телефона");
     }
 }
