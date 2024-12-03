4b4d8af99e9bb052a3a5623e38c7f84478fa3512
Mits16 <mits1388@gmail.com>
2024-12-03 19:48:52 +0300

    add class LoginMessage

---
+++ src/main/java/by/dominos/api/LoginMessage.java
@@ -0,0 +1,7 @@
+package by.dominos.api;
+
+public class LoginMessage {
+    public static final String LOGIN_EMPTY_NUMBER_PHONE = "Это поле не может быть пустым.";
+    public static final String LOGIN_INCORRECT_NUMBER_PHONE = "Введите корректный номер телефона.";
+    public static final String LOGIN_CORRECT_NUMBER_PHONE = "Введите, пожалуйста, код подтверждения, присланный на номер +375298564032";
+}
