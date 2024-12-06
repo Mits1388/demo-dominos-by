4824f6a6542de2a1adce07a6f103f782b734dc9d
Mits16 <mits1388@gmail.com>
2024-12-06 19:20:08 +0300

    add class SwitchingToSitePage

---
+++ src/main/java/by/dominos/ui/pages/SwitchingToSitePage.java
@@ -0,0 +1,33 @@
+package by.dominos.ui.pages;
+
+import by.dominos.ui.webdriver.Singleton;
+import by.dominos.ui.xpath.SwitchingToSiteXpath;
+import by.dominos.utils.ScrollToBoottom;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SwitchingToSitePage {
+    private WebDriver driver;
+
+    public SwitchingToSitePage() {
+        this.driver = Singleton.getDriver();
+    }
+
+    public SwitchingToSitePage clickingToInstagram(){
+        new ScrollToBoottom().scrollToBoottom(driver);
+        driver.findElement(By.xpath(SwitchingToSiteXpath.CLICK_INSTAGRAM_XPATH)).click();
+        return this;
+    }
+
+    public SwitchingToSitePage clickingToFacebook() {
+        new ScrollToBoottom().scrollToBoottom(driver);
+        driver.findElement(By.xpath(SwitchingToSiteXpath.CLICK_FACEBOOK_XPATH)).click();
+        return this;
+    }
+
+    public SwitchingToSitePage clickingToTiktok() {
+        new ScrollToBoottom().scrollToBoottom(driver);
+        driver.findElement(By.xpath(SwitchingToSiteXpath.CLICK_TIKTOK_XPATH)).click();
+        return this;
+    }
+}
