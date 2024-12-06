3621705100f8dcad61b446c5369f166496870d02
Mits16 <mits1388@gmail.com>
2024-12-06 19:19:50 +0300

    add class SwitchingToSiteMessage

---
+++ src/main/java/by/dominos/ui/messages/SwitchingToSiteMessage.java
@@ -0,0 +1,7 @@
+package by.dominos.ui.messages;
+
+public class SwitchingToSiteMessage {
+    public static final String INSTAGRAM_LINK = "https://www.instagram.com/dominosby/";
+    public static final String FACEBOOK_LINK = "https://www.facebook.com/dominosby/";
+    public static final String TIKTOK_LINK = "https://www.tiktok.com/@dominosby";
+}
