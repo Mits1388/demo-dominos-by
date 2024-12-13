00ec13c5745a3eb9a748a79eff6b120c4806d7d5
Mits16 <mits1388@gmail.com>
2024-12-13 20:56:25 +0300

    add class PizzaConstructorXpath

---
+++ src/main/java/by/dominos/ui/xpath/PizzaConstructorXpath.java
@@ -0,0 +1,19 @@
+package by.dominos.ui.xpath;
+
+public class PizzaConstructorXpath {
+    public static final String CLICK_BURGER_XPATH = "//*[@id=\"root\"]/div/header/div/div[4]/div/div/label/div";
+    public static final String CLICK_CONSTRUCTOR_PIZZA_XPATH = "/html/body/div[1]/div/header/div/div[4]/div/div/div/div/div/div[2]/div/ul/li[2]/a";
+    public static final String CLICK_MEAT_XPATH = "//*[@id=\"root\"]/div/div[2]/div/div/div[2]/div[1]/div[2]/div[1]";
+    public static final String CLICK_SEAFOOD_XPATH = "//*[@id=\"root\"]/div/div[2]/div/div/div[2]/div[1]/div[2]/div[2]";
+    public static final String CLICK_VEGETABLES_XPATH = "//*[@id=\"root\"]/div/div[2]/div/div/div[2]/div[1]/div[2]/div[3]";
+    public static final String CLICK_PRODUCTS_XPATH = "//*[@id=\"root\"]/div/div[2]/div/div/div[2]/div[1]/div[2]/div[4]";
+    public static final String CLICK_CHEESE_XPATH = "//*[@id=\"root\"]/div/div[2]/div/div/div[2]/div[1]/div[2]/div[5]";
+    public static final String CLICK_SAUCES_XPATH = "//*[@id=\"root\"]/div/div[2]/div/div/div[2]/div[1]/div[2]/div[6]";
+    public static final String CLICK_CHICKEN_XPATH = "//*[@id=\"root\"]/div/div[2]/div/div/div[2]/div[1]/div[2]/div[1]/div[2]/div[2]/button";
+    public static final String CLICK_SHRIMPS_XPATH = "//*[@id=\"root\"]/div/div[2]/div/div/div[2]/div[1]/div[2]/div[2]/div[2]/div/button";
+    public static final String CLICK_SWEET_PEPPER_XPATH = "//*[@id=\"root\"]/div/div[2]/div/div/div[2]/div[1]/div[2]/div[3]/div[2]/div[2]/button";
+    public static final String CLICK_PINEAPPLE_XPATH = "/html/body/div[1]/div/div[2]/div/div/div[2]/div[1]/div[2]/div[4]/div[2]/div[1]/button";
+    public static final String CLICK_MINUS_MOZZARELLA_CHEESE_XPATH = "//*[@id=\"root\"]/div/div[2]/div/div/div[2]/div[1]/div[2]/div[5]/div[2]/div[2]/div[3]/button[1]";
+    public static final String CLICK_MINUS_DOMINOS_TOMATO_SAUCE_XPATH = "//*[@id=\"root\"]/div/div[2]/div/div/div[2]/div[1]/div[2]/div[6]/div[2]/div[4]/div[3]/button[1]";
+    public static final String CLICK_FRIED_ONIONS_XPATH = "//*[@id=\"root\"]/div/div[2]/div/div/div[2]/div[1]/div[2]/div[4]/div[2]/div[3]/button";
+}
