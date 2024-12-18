1f8606bc549b7d7dfc4b95d0077c4a35b5d035d1
Mits16 <mits1388@gmail.com>
2024-12-18 19:50:22 +0300

    add class PizzaConstructorMessage

---
+++ src/main/java/by/dominos/ui/messages/PizzaConstructorMessage.java
@@ -0,0 +1,11 @@
+package by.dominos.ui.messages;
+
+public class PizzaConstructorMessage {
+    public static final String CHICKEN_MESSAGE = "Курица x1";
+    public static final String SHRIMPS_MESSAGE = "Креветки x1";
+    public static final String SWEET_PEPPER_MESSAGE = "Сладкий перец x1";
+    public static final String PINEAPPLE_MESSAGE = "Ананас x1";
+    public static final String MOZZARELLA_CHEESE_MESSAGE = "Сыр моцарелла x1";
+    public static final String DOMINOS_TOMATO_SAUCE_MESSAGE = "Томатный соус Domino's x1";
+    public static final String FRIED_ONIONS_MESSAGE = "Обжаренный лук x1";
+}
