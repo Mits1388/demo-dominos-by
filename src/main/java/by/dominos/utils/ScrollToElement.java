28a61eb0949d649ad2c7853d901b3118d65627ed
Mits16 <mits1388@gmail.com>
2024-12-21 21:41:35 +0300

    add class ScrollToElement

---
+++ src/main/java/by/dominos/utils/ScrollToElement.java
@@ -0,0 +1,18 @@
+package by.dominos.utils;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ScrollToElement {
+
+    public void scrollToElement(WebDriver driver, WebElement element) {
+
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();",element);
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
