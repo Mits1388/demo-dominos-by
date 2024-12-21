4846f4076d637da8555bb4840be468f75166945c
Mits16 <mits1388@gmail.com>
2024-12-21 21:19:06 +0300

    refactoring class OrderXpath

--- src/main/java/by/dominos/ui/xpath/OrderXpath.java
+++ src/main/java/by/dominos/ui/xpath/OrderXpath.java
@@ -7,10 +7,16 @@ public class OrderXpath {
                     //"//*[@id=\"425a3e7e-f158-4519-8f19-7ee1f6f398d8\"]/div[2]/div[2]/div[2]/button[1]/div";
     public static final String BUTTON_IN_BASKET_CHICKEN_WITH_CHAMPIGNONS_XPATH = "//*[@id=\"425a3e7e-f158-4519-8f19-7ee1f6f398d8\"]//button//div[text()='В корзину']";
                             //"//*[@id=\"425a3e7e-f158-4519-8f19-7ee1f6f398d8\"]/div[3]/div/div[2]/button";
-    public static final String BUTTON_BASKET_XPATH = "//*[@id=\"root\"]/div/div[1]/div/div/div/div/a";
-    public static final String ORDER_CHICKEN_WITH_CHAMPIGNONS_XPATH = "//*[@id=\"root\"]/div/div[2]/div/div/div[4]/div[2]/div/div[2]/ul/li/div[1]/span[1]";
-    public static final String ORDER_SIZE_AND_BASE_PIZZA_XPATH = "/html/body/div[1]/div/div[2]/div/div/div[4]/div[2]/div/div[2]/ul/li/div[2]";
-    public static final String BUTTON_CLOSE_XPATH = "/html/body/div[7]/div/div/div/div/button";
+    public static final String BUTTON_BASKET_XPATH = " //a[text()='Заказать']";
+
+    //*[@id="root"]/div/div[1]/div/div/div/div/a
+
+    public static final String ORDER_CHICKEN_WITH_CHAMPIGNONS_XPATH = "//*[@id=\"root\"]//div[2]//span[text()='Цыпленок с шампиньонами']";
+            //"//*[@id=\"root\"]/div/div[2]/div/div/div[4]/div[2]/div/div[2]/ul/li/div[1]/span[1]";
+    //div[contains(@class, 'yourClassName')]//span[1]
+    public static final String ORDER_SIZE_AND_BASE_PIZZA_XPATH = "//div[2]//div[text()='L, Классическая New']";
+                    //"/html/body/div[1]/div/div[2]/div/div/div[4]/div[2]/div/div[2]/ul/li/div[2]";
+
+    public static final String BUTTON_CLOSE_XPATH = "//button[contains(@class, 'base-tab-button close-icon-button app-modal__close')]";
+            //"/html/body/div[7]/div/div/div/div/button";
 }
-//div[@id="425a3e7e-f158-4519-8f19-7ee1f6f398d8"]//button[2][text()='Классическая New']
-//*[@id="425a3e7e-f158-4519-8f19-7ee1f6f398d8"]//button[2]//div[text()='Классическая New']
