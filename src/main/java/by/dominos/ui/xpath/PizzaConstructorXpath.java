df563335763b5c12733e79a2470d611bd41180bd
Mits16 <mits1388@gmail.com>
2024-12-23 20:43:31 +0300

    refactoring

--- src/main/java/by/dominos/ui/xpath/PizzaConstructorXpath.java
+++ src/main/java/by/dominos/ui/xpath/PizzaConstructorXpath.java
@@ -2,20 +2,20 @@ package by.dominos.ui.xpath;
 
 public class PizzaConstructorXpath {
     public static final String CLICK_BURGER_XPATH = "/html/body/div[1]/div/header/div/div[4]";
-    public static final String CLICK_CONSTRUCTOR_PIZZA_XPATH = "//a[text()='Конструктор пиццы'and @href='https://dominos.by/ru/minsk/create/']";
+    public static final String CLICK_CONSTRUCTOR_PIZZA_XPATH = "//a[text()= \"Конструктор пиццы\" and @href=\"https://dominos.by/ru/minsk/create/\"]";
     public static final String CLICK_MEAT_XPATH = "//div[text() = \"Мясо\"]";
     public static final String CLICK_SEAFOOD_XPATH = "//div[text() = \"Морепродукты\"]";
     public static final String CLICK_VEGETABLES_XPATH = "//div[text() = \"Овощи\"]";
     public static final String CLICK_PRODUCTS_XPATH = "//div[text() = \"Продукты\"]";
     public static final String CLICK_CHEESE_XPATH = "//div[text() = \"Сыр\"]";
     public static final String CLICK_SAUCES_XPATH = "//div[text() = \"Соусы\"]";
-    public static final String CLICK_CHICKEN_XPATH = "(//div[@class='topping-items'])[1]//div[2]//button[1]";
-    public static final String CLICK_SHRIMPS_XPATH = "(//div[@class='topping-items'])[1]//div[1]//button[1]";
-    public static final String CLICK_SWEET_PEPPER_XPATH = "(//div[@class='topping-items'])[2]//div[2]//button[1]";
-    public static final String CLICK_PINEAPPLE_XPATH = "(//div[@class='topping-items'])[2]//div[1]//button[1]";
-    public static final String CLICK_MINUS_MOZZARELLA_CHEESE_XPATH = "(//div[@class='topping-items'])[3]//div[2]//button[1]";
-    public static final String CLICK_MINUS_DOMINOS_TOMATO_SAUCE_XPATH = "(//div[@class='topping-items'])[4]//div[4]//button[1]";
-    public static final String CLICK_FRIED_ONIONS_XPATH = "(//div[@class='topping-items'])[3]//div[3]//button[1]";
+    public static final String CLICK_CHICKEN_XPATH = "(//div[@class= \"topping-items\"])[1]//div[2]//button[1]";
+    public static final String CLICK_SHRIMPS_XPATH = "(//div[@class= \"topping-items\"])[1]//div[1]//button[1]";
+    public static final String CLICK_SWEET_PEPPER_XPATH = "(//div[@class= \"topping-items\"])[2]//div[2]//button[1]";
+    public static final String CLICK_PINEAPPLE_XPATH = "(//div[@class= \"topping-items\"])[2]//div[1]//button[1]";
+    public static final String CLICK_MINUS_MOZZARELLA_CHEESE_XPATH = "(//div[@class= \"topping-items\"])[3]//div[2]//button[1]";
+    public static final String CLICK_MINUS_DOMINOS_TOMATO_SAUCE_XPATH = "(//div[@class= \"topping-items\"])[4]//div[4]//button[1]";
+    public static final String CLICK_FRIED_ONIONS_XPATH = "(//div[@class= \"topping-items\"])[3]//div[3]//button[1]";
     public static final String CHICKEN_XPATH = "//div[text() = \"Курица x1\"]";
     public static final String SHRIMPS_XPATH = "//div[text() = \"Креветки x1\"]";
     public static final String SWEET_PEPPER_XPATH = "//div[text() = \"Сладкий перец x1\"]";
--- src/main/java/by/dominos/ui/xpath/SwitchingToSiteXpath.java
+++ src/main/java/by/dominos/ui/xpath/SwitchingToSiteXpath.java
@@ -1,7 +1,7 @@
 package by.dominos.ui.xpath;
 
 public class SwitchingToSiteXpath {
-    public static final String CLICK_INSTAGRAM_XPATH = "//a[@href='https://www.instagram.com/dominosby/']";
-    public static final String CLICK_FACEBOOK_XPATH = "//*[@id=\"footer\"]//*[@href='https://www.facebook.com/dominosby/']";
-    public static final String CLICK_TIKTOK_XPATH = "//*[@id=\"footer\"]//*[@href='https://www.tiktok.com/@dominosby']";
+    public static final String CLICK_INSTAGRAM_XPATH = "//a[@href= \"https://www.instagram.com/dominosby/\"]";
+    public static final String CLICK_FACEBOOK_XPATH = "//*[@id=\"footer\"]//*[@href= \"https://www.facebook.com/dominosby/\"]";
+    public static final String CLICK_TIKTOK_XPATH = "//*[@id=\"footer\"]//*[@href= \"https://www.tiktok.com/@dominosby\"]";
 }
