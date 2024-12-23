a7d44688facbb65162de253bccdcbefb3b797595
Mits16 <mits1388@gmail.com>
2024-12-23 21:47:58 +0300

    refactoring

--- src/main/java/by/dominos/ui/pages/OrderPage.java
+++ src/main/java/by/dominos/ui/pages/OrderPage.java
@@ -2,9 +2,15 @@ package by.dominos.ui.pages;
 
 import by.dominos.ui.xpath.OrderXpath;
 import by.dominos.ui.webdriver.Singleton;
+import by.dominos.ui.xpath.PizzaConstructorXpath;
+import by.dominos.utils.ScrollToElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class OrderPage {
     private final WebDriver driver;
@@ -14,49 +20,66 @@ public class OrderPage {
     }
 
     public OrderPage clickSizeL() {
-        By by = By.xpath(OrderXpath.SIZE_L_CHICKEN_WITH_CHAMPIGNONS_XPATH);
-        WebElement element = driver.findElement(by);
-        element.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(OrderXpath.SIZE_L_CHICKEN_WITH_CHAMPIGNONS_XPATH))).click();
+        //By by = By.xpath(OrderXpath.SIZE_L_CHICKEN_WITH_CHAMPIGNONS_XPATH);
+        //WebElement element = driver.findElement(by);
+       // element.click();
         return this;
     }
 
     public OrderPage clickClassicPizza() {
-        By by = By.xpath(OrderXpath.CLASSIC_CHICKEN_WITH_CHAMPIGNONS_XPATH);
-        WebElement element = driver.findElement(by);
-        element.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(OrderXpath.CLASSIC_CHICKEN_WITH_CHAMPIGNONS_XPATH))).click();
+       // By by = By.xpath(OrderXpath.CLASSIC_CHICKEN_WITH_CHAMPIGNONS_XPATH);
+       // WebElement element = driver.findElement(by);
+       // element.click();
         return this;
     }
 
     public OrderPage clickInBasket() {
-        By by = By.xpath(OrderXpath.BUTTON_IN_BASKET_CHICKEN_WITH_CHAMPIGNONS_XPATH);
-        WebElement element = driver.findElement(by);
-        element.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(OrderXpath.BUTTON_IN_BASKET_CHICKEN_WITH_CHAMPIGNONS_XPATH))).click();
+        //By by = By.xpath(OrderXpath.BUTTON_IN_BASKET_CHICKEN_WITH_CHAMPIGNONS_XPATH);
+        //WebElement element = driver.findElement(by);
+        //element.click();
         return this;
     }
 
     public OrderPage clickBasket() {
-        By by = By.xpath(OrderXpath.BUTTON_BASKET_XPATH);
-        WebElement element = driver.findElement(by);
-        element.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(OrderXpath.BUTTON_BASKET_XPATH))).click();
+        //By by = By.xpath(OrderXpath.BUTTON_BASKET_XPATH);
+        //WebElement element = driver.findElement(by);
+        //element.click();
         return this;
     }
 
     public OrderPage clickClose() {
-        By by = By.xpath(OrderXpath.BUTTON_CLOSE_XPATH);
-        WebElement element = driver.findElement(by);
-        element.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(OrderXpath.BUTTON_CLOSE_XPATH))).click();
+        //By by = By.xpath(OrderXpath.BUTTON_CLOSE_XPATH);
+        //WebElement element = driver.findElement(by);
+        //element.click();
         return this;
     }
 
     public String orderNamePizza() {
-        By by = By.xpath(OrderXpath.ORDER_CHICKEN_WITH_CHAMPIGNONS_XPATH);
-        WebElement element = driver.findElement(by);
+        WebElement element = driver.findElement(By.xpath(OrderXpath.ORDER_CHICKEN_WITH_CHAMPIGNONS_XPATH));
+        new ScrollToElement().scrollToElement(driver, element);
         return element.getText();
+
+        //By by = By.xpath(OrderXpath.ORDER_CHICKEN_WITH_CHAMPIGNONS_XPATH);
+       // WebElement element = driver.findElement(by);
+       // return element.getText();
     }
 
     public String orderSizeAndBasePizza() {
-        By by = By.xpath(OrderXpath.ORDER_SIZE_AND_BASE_PIZZA_XPATH);
-        WebElement element = driver.findElement(by);
+        WebElement element = driver.findElement(By.xpath(OrderXpath.ORDER_SIZE_AND_BASE_PIZZA_XPATH));
+        new ScrollToElement().scrollToElement(driver, element);
         return element.getText();
+        //By by = By.xpath(OrderXpath.ORDER_SIZE_AND_BASE_PIZZA_XPATH);
+        //WebElement element = driver.findElement(by);
+        //return element.getText();
     }
 }
