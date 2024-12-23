66edc1178a182cf79a775d54f36b29b7cd56e267
Mits16 <mits1388@gmail.com>
2024-12-23 21:39:53 +0300

    refactoring

--- src/main/java/by/dominos/ui/xpath/OrderXpath.java
+++ src/main/java/by/dominos/ui/xpath/OrderXpath.java
@@ -1,11 +1,11 @@
 package by.dominos.ui.xpath;
 
 public class OrderXpath {
-    public static final String SIZE_L_CHICKEN_WITH_CHAMPIGNONS_XPATH = "//*[@id=\"425a3e7e-f158-4519-8f19-7ee1f6f398d8\"]//button[2]//div[text()= \"L\"]";
-    public static final String CLASSIC_CHICKEN_WITH_CHAMPIGNONS_XPATH = "//*[@id=\"425a3e7e-f158-4519-8f19-7ee1f6f398d8\"]//button[1]//div[text()= \"Классическая New\"]";
-    public static final String BUTTON_IN_BASKET_CHICKEN_WITH_CHAMPIGNONS_XPATH = "//*[@id=\"425a3e7e-f158-4519-8f19-7ee1f6f398d8\"]//button//div[text()= \"В корзину\"]";
-    public static final String BUTTON_BASKET_XPATH = "//a[text()= \"Заказать\"]";
-    public static final String ORDER_CHICKEN_WITH_CHAMPIGNONS_XPATH = "//*[@id=\"root\"]//div[2]//span[text()= \"Цыпленок с шампиньонами\"]";
-    public static final String ORDER_SIZE_AND_BASE_PIZZA_XPATH = "//div[2]//div[text()= \"L, Классическая New\"]";
-    public static final String BUTTON_CLOSE_XPATH = "//button[contains(@class, \"base-tab-button close-icon-button app-modal__close\")]";
+    public static final String SIZE_L_CHICKEN_WITH_CHAMPIGNONS_XPATH = "//*[@id=\"425a3e7e-f158-4519-8f19-7ee1f6f398d8\"]//button[2]//div[@class ='inner-block' and text()='L']";
+    public static final String CLASSIC_CHICKEN_WITH_CHAMPIGNONS_XPATH = "//*[@id=\"425a3e7e-f158-4519-8f19-7ee1f6f398d8\"]//button[1]//div[text()='Классическая New']";
+    public static final String BUTTON_IN_BASKET_CHICKEN_WITH_CHAMPIGNONS_XPATH = "//*[@id=\"425a3e7e-f158-4519-8f19-7ee1f6f398d8\"]//button//div[text()='В корзину']";
+    public static final String BUTTON_BASKET_XPATH = "//a[text()='Заказать']";
+    public static final String ORDER_CHICKEN_WITH_CHAMPIGNONS_XPATH = "//*[@id=\"root\"]//div[2]//span[text()='Цыпленок с шампиньонами']";
+    public static final String ORDER_SIZE_AND_BASE_PIZZA_XPATH = "//div[2]//div[text()='L, Классическая New']";
+    public static final String BUTTON_CLOSE_XPATH = "//button[contains(@class, 'base-tab-button close-icon-button app-modal__close')]";
 }
